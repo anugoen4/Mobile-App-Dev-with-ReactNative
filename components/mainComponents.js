192292af7c934e17d62ad57519179e8da4b54d6c
Anurag <anuraggoenka25@gmail.com>
2020-06-19 23:01:28 +0530

    Adding Contact Us and About Us Components

--- components/mainComponents.js
+++ components/mainComponents.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Menu from  './menuComponents'
 import Home from './homeComponent';
+import Contact from './ContactUs';
+import About from './AboutUs';
 import {View, FlatList} from 'react-native';
 import DishDetail from './dishdetailComponent';
 import {createStackNavigator, createDrawerNavigator} from 'react-navigation';
@@ -36,6 +38,34 @@ const HomeNavigator = createStackNavigator({
     }
 });
 
+const ContactUsNavigator = createStackNavigator({
+    ContactUs: {screen: Contact}
+},{
+    navigationOptions:{
+        headerStyle:{
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color: '#fff'
+        }
+    }
+});
+
+const AboutUsNavigator = createStackNavigator({
+    AboutUs: {screen: About}
+},{
+    navigationOptions:{
+        headerStyle:{
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color: '#fff'
+        }
+    }
+});
+
 const MainNavigator = createDrawerNavigator({
     Home: {
         screen: HomeNavigator,
@@ -44,11 +74,32 @@ const MainNavigator = createDrawerNavigator({
             drawerLabel: 'Home'
         }
     },
+    AboutUs: {
+        screen: AboutUsNavigator,
+        navigationOptions: {
+            title: 'About Us',
+            drawerLabel: 'About Us'
+        }
+    },
     Menu: {
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
             drawerLabel: 'Menu'
+        }
+    },
+    ContactUs: {
+        screen: ContactUsNavigator,
+        navigationOptions:{
+            title: 'Contact Us',
+            drawerLabel: 'Contact Us'
+        }
+    },
+    AboutUs: {
+        screen: AboutUsNavigator,
+        navigationOptions: {
+            title: 'About Us',
+            drawerLabel: 'About Us'
         }
     }
 },{
